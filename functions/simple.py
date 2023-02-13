d85fe92900974009ea048349cd6ae7a404291347
Amir Mohammad Tavakkoli <tavakkoli.amirmohammad@gmail.com>
2023-02-13 09:19:38 -0700

    Add function exmaple

---
+++ functions/simple.py
@@ -0,0 +1,14 @@
+# An example show how to use uninterpreted function
+from z3 import *
+
+x = Int('x')
+y = Int('y')
+f = Function('f', IntSort(), IntSort())
+
+s = Solver()
+s.add(f(f(x)) == x, f(x) == y, x != y)
+print(s.check())
+
+m = s.model()
+print("f(f(x)) =", m.evaluate(f(f(x))))
+print("f(x)    =", m.evaluate(f(x)))
